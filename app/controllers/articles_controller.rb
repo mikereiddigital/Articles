416857a4cf7e50dfdc009eb3a9dd4278e3c61cd3
mikereiddigital <mike.reid@digital.justice.gov.uk>
2017-02-24 14:53:09 +0000

    Changes on 24 Feb 2017

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,21 @@ def new
     @article=Article.new
 end
 
+def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+
+def edit
+  @article = Article.find(params[:id])
+end
+
+
 private
   def article_params
     params.require(:article).permit(:title, :text)
